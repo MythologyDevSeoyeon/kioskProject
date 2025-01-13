e534317b4a4755c114f616b3054a426cc9756dc5
Seoyeon <dev_seoyeon@mythologyteams.com>
2025-01-13 17:03:45 +0900

    Feat: Kiosk 클래스 생성

---
+++ src/main/java/com/example/Level3/MenuItem.java
@@ -0,0 +1,19 @@
+package com.example.Level3;
+
+public class MenuItem {
+    //필드
+    String name;
+    double price;
+    String description;
+
+    //생성자
+    MenuItem(String name, double price, String description){
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    public MenuItem() {
+
+    }
+}
