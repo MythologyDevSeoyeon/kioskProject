bb1b78345a67e89643c824ccb60a5330eb7ab08c
Seoyeon <dev_seoyeon@mythologyteams.com>
2025-01-13 11:24:46 +0900

    Initial commit

--- src/main/java/com/example/Level1/Main.java
+++ src/main/java/com/example/Level1/Main.java
@@ -1,7 +1,7 @@
-package com.example;
+package com.example.Level1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello Java");
     }
 }
