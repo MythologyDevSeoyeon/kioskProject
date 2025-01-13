aa15cb0a6d31cdf900ab3fc7dc0e77d1b59358f7
Seoyeon <dev_seoyeon@mythologyteams.com>
2025-01-13 21:04:47 +0900

    Refactor: 리팩토링

--- src/main/java/com/example/Level2/Main.java
+++ src/main/java/com/example/Level2/Main.java
@@ -7,23 +7,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         List<MenuItem> menuItems = new ArrayList<>();
 
-        MenuItem item1 = new MenuItem("ShackBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거");
-        MenuItem item2 = new MenuItem("SmokeShack", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거");
-        MenuItem item3 = new MenuItem("Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
-        MenuItem item4 = new MenuItem("Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거");
-
-        menuItems.add(item1);
-        menuItems.add(item2);
-        menuItems.add(item3);
-        menuItems.add(item4);
+        menuItems.add(new MenuItem("ShackBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
+        menuItems.add(new MenuItem("SmokeShack", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
+        menuItems.add(new MenuItem("Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
+        menuItems.add(new MenuItem("Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거"));
 
         //반복문을 이용하여 메뉴 출력
         System.out.println("[ SHAKESHACK MENU ]");
         for (int i = 0; i < menuItems.size(); i++) {
             MenuItem item = menuItems.get(i);
-            System.out.println((i + 1) + ". " + item.name + " | W " + item.price + " | " + item.description);
+            System.out.printf("%-3d %-15s | W %-4.1f | %s\n", i + 1, item.name, item.price, item.description);
         }
 
         Scanner sc = new Scanner(System.in);
