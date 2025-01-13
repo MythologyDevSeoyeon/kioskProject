dde950ebec868082ac2522f433b5add5ddf429cd
Seoyeon <dev_seoyeon@mythologyteams.com>
2025-01-13 22:10:30 +0900

    Refactor: 리팩토링

--- src/main/java/com/example/Level1/Main.java
+++ src/main/java/com/example/Level1/Main.java
@@ -6,16 +6,30 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("[ SHAKESHACK MENU ]");
-
         String[][] menuItems = dataInitialize();
 
+        System.out.println("[ SHAKESHACK MENU ]");
         for (String[] item : menuItems) {
             System.out.printf("%-3s %-15s | W %-4s | %s\n", item[0], item[1], item[2], item[3]);
         }
 
+        run();
+
+    }
+
+    //데이터 초기화
+    public static String[][] dataInitialize() {
+        return new String[][]{
+                {"1.", "ShackBurger", "6.9", "토마토, 양상추, 쉑소스가 토핑된 치즈버거"},
+                {"2.", "SmokeShack", "8.9", "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"},
+                {"3.", "Cheeseburger", "6.9", "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"},
+                {"4.", "Hamburger", "5.4", "비프패티를 기반으로 야채가 들어간 기본버거"}
+        };
+    }
+
+    //실행 메소드
+    public static void run() {
+        Scanner sc = new Scanner(System.in);
         while (true) {
             try {
                 System.out.print("주문하실 ShackBurger Menu 번호를 입력하세요 (0 입력 시 종료): ");
@@ -47,15 +61,5 @@ public class Main {
                 sc.next();
             }
         }
-    }
-
-    //데이터 초기화
-    public static String[][] dataInitialize() {
-        return new String[][]{
-                {"1.", "ShackBurger", "6.9", "토마토, 양상추, 쉑소스가 토핑된 치즈버거"},
-                {"2.", "SmokeShack", "8.9", "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"},
-                {"3.", "Cheeseburger", "6.9", "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"},
-                {"4.", "Hamburger", "5.4", "비프패티를 기반으로 야채가 들어간 기본버거"}
-        };
     }
 }
