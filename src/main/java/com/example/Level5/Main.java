9f727b7846b3c352af2b46c51c20a97dfbdef0d3
Seoyeon <dev_seoyeon@mythologyteams.com>
2025-01-15 16:03:28 +0900

    Comment: 주석 수정

--- src/main/java/com/example/Level5/Main.java
+++ src/main/java/com/example/Level5/Main.java
@@ -5,13 +5,17 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
+        //Menu리스트에 초기값 추가
         List<Menu> menus =initializeMenu();
-        Kiosk kiosk = new Kiosk(menus);
-        kiosk.start();
 
+        //kiosk 객체 생성
+        Kiosk kiosk = new Kiosk(menus);
+
+        //kiosk 시작 메소드
+        kiosk.start();
     }
 
-    //메뉴의 리스트 생성 및 데이터 추가
+    //메뉴의 리스트 생성 및 데이터 초기화
     public static List<Menu> initializeMenu() {
         List<Menu> menus = new ArrayList<>();
         menus.add(new Menu("Hamburgers", createHamburgers()));
@@ -20,7 +24,7 @@ public class Main {
         return menus;
     }
 
-    //hamburger 메뉴의 MenuItem 리스트 생성 및 데이터 추가
+    //hamburger 메뉴의 MenuItem 리스트 생성 및 데이터 초기화
     public static List<MenuItem> createHamburgers() {
         List<MenuItem> hamburgers = new ArrayList<>();
         hamburgers.add(new MenuItem("ShackBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
@@ -30,7 +34,7 @@ public class Main {
         return hamburgers;
     }
 
-    //drink 메뉴의 MenuItem 리스트 생성 및 데이터 추가
+    //drink 메뉴의 MenuItem 리스트 생성 및 데이터 초기화
     public static List<MenuItem> createDrinks() {
         List<MenuItem> drinks = new ArrayList<>();
         drinks.add(new MenuItem("Coke", 2.5, "탄산음료"));
@@ -40,7 +44,7 @@ public class Main {
         return drinks;
     }
 
-    //dessert 메뉴의 MenuItem 리스트 생성 및 데이터 추가
+    //dessert 메뉴의 MenuItem 리스트 생성 및 데이터 초기화
     public static List<MenuItem> createDesserts() {
         List<MenuItem> desserts = new ArrayList<>();
         desserts.add(new MenuItem("Cookie", 2.8, "바삭한 쿠키"));
